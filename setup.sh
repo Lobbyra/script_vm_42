27688a8d15feea5aa450a500bd17a735db7ec516
lobbyra <jecaudal@student.42.fr>
2020-07-29 20:34:52 +0200

    first commit should be good script

--- setup.sh
+++ setup.sh
@@ -1,17 +1,50 @@
+#!/bin/bash
+
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 NC='\033[0m'
+
+if [ "$EUID" -ne 0 ]
+then
+	printf "${ORANGE}WARNING : ${NC}Please run this script with sudo. :)\n"
+	exit
+fi
 
 echo "user42\nuser42" | sudo passwd root > /dev/null
 clear
 printf "${GREEN}root password changed to user42.${NC}\n"
 
+# This down code is to disable daily update that can lock apt use at start
+# and for a long time... Do apt update sometimes.
 sudo echo "APT::Periodic::Update-Package-Lists \"0\";" > /etc/apt/apt.conf.d/20auto-upgrades
 sudo echo "APT::Periodic::Unattended-Upgrade \"1\";" >> /etc/apt/apt.conf.d/20auto-upgrades
 
+echo "
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+
+<channel name=\"keyboard-layout\" version=\"1.0\">
+  <property name=\"Default\" type=\"empty\">
+    <property name=\"XkbDisable\" type=\"bool\" value=\"false\"/>
+    <property name=\"XkbModel\" type=\"string\" value=\"macintosh\"/>
+    <property name=\"XkbLayout\" type=\"string\" value=\"us\"/>
+    <property name=\"XkbVariant\" type=\"string\" value=\"mac\"/>
+  </property>
+</channel>
+" > $HOME/.config/xfce4/xfconf/xfce-perchannel-xml/keyboard-layout.xml
+
 printf "${GREEN}Keyboard layout changed to macintosh US${NC}\n"
 
-sudo apt -qq update
+sudo apt -qq update &
+PID=$!
+i=1
+sp="/-\|"
+echo -n 'Research and installation of some updates...'
+printf "\n"
+while [ -d /proc/$PID ]
+do
+	printf "\r${sp:i++%${#sp}:1}"
+	sleep 0.2
+done
 
 printf "${GREEN}Apt update done, you can upgrade but it could be long.${NC}\n"
 
@@ -28,19 +61,12 @@ set autoindent" > $HOME/.vimrc
 
 printf "${GREEN}Vim config installed${NC}\n"
 
-runuser -l user42 -c 'code --install-extension shan.code-settings-sync &> /dev/null'
+runuser -l user42 -c 'code --install-extension kube.42header &> /dev/null'
 
-printf "${GREEN}VS Code Sync extension installed${NC}\n"
-
-echo "{
-    "sync.gist": "a3bcc1e7560714caf1c91c7f073567ab",
-    "sync.forceDownload": true,
-    "sync.autoDownload": true
-}" > $HOME/.config/Code/User/settings.json
-
-printf "${GREEN}Sync setup${NC}\n"
+printf "${GREEN}VS Code 42 Header extension installed${NC}\n"
 
 mkdir $HOME/code 2> /dev/null
+chown user42 $HOME/code
 printf "${GREEN}mkdir $HOME/code${NC}\n"
 
 echo "cd $HOME/code" >> $HOME/.zshrc
